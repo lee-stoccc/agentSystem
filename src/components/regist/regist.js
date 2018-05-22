dfc9d33d918836fa1c419ff98b00fc03a72ec429
shadow <870722422@qq.com>
2018-05-22 08:58:22 +0800

    xx

--- src/components/regist/regist.js
+++ src/components/regist/regist.js
@@ -18,25 +18,6 @@ export default {
     methods:{
         sub:function () {
             this.http('http://192.168.1.161/register',{companyName:this.companyName,phone:this.phone,code:this.code,password:this.password,inviteCode:this.inviteCode})
-            // this.$axios.post('http://192.168.1.161/register',
-            //     {companyName:this.companyName,phone:this.phone,code:this.code,password:this.password,inviteCode:this.inviteCode}
-            // ,{
-            //     transformRequest: [function (data) {
-            //         // 对 data 进行任意转换处理
-            //         let ret = '';
-            //         for (let it in data){
-            //             ret += encodeURIComponent(it)+'='+encodeURIComponent(data[it])+'&'
-            //         }
-            //         return ret
-            //         // return this.$qs.stringify(data);
-            //     }],
-            //     headers:{
-            //         "Content-Type":'application/x-www-form-urlencoded;charset=UTF-8'
-            //     }
-            // }).then(res=>{
-            //     console.log(2111111111111);
-            //     console.log(res)
-            // })
         }
     },
     mounted:function () {
